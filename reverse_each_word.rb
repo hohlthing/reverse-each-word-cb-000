8a8ca277a29ec7037f429c1ddba412c3099db1ba
michael hohl <mhohl@rocketmail.com>
2018-03-19 20:50:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,10 @@
 def reverse_each_word(string)
-  string.split.collect { |x| x.reverse }.join(" ")
+  array = string.split
+  words_reversed = []
+  array.each { |x| words_reversed << x.reverse}.join
 end
+
+#
+# def reverse_each_word(string)
+#   string.split.collect { |x| x.reverse }.join(" ")
+# end
